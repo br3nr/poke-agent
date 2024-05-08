9f514e8f0c7429e88f02754142e862d4977c3d3a
br3nr <repetitive_routine@protonmail.com>
2024-05-08 21:08:40 +0800

    fix permalock due to default user-agent

--- showdown.py
+++ showdown.py
@@ -16,29 +16,32 @@ async def get_challenge_data(challstr):
         "pass": PASSWORD,
         "challstr": challstr,
     }
-
+    headers = {
+        'User-Agent': 'PokeAgentv1'
+    }
     uri = "https://play.pokemonshowdown.com/api/login"
-
-    response = requests.post(uri, data=payload)
-    json_str = response.content.decode('utf-8')  # Decoding the bytes object to a string
+    response = requests.post(uri, data=payload, headers=headers)
+    json_str = response.content.decode('utf-8')
     data = json.loads(json_str[1:])
     return data
 
-
 async def showdown_client():
+    headers = {
+        'User-Agent': 'PokeAgentv1'
+    }
     url = 'wss://sim3.psim.us/showdown/websocket'
-    
-    async with websockets.connect(url) as websocket:
+    async with websockets.connect(url, extra_headers=headers) as websocket:
         async for message in websocket:
             print(message)
-            if ("challstr" in message):
+            if "challstr" in message:
                 data = await get_challenge_data(message[10:])
                 assert_str = f"|/trn {USERNAME},0,{data['assertion']}" 
                 print(assert_str)
                 await websocket.send(assert_str)
-
-                #search_battle = "|/challenge br3nr, gen7randombattle"
-                #await websocket.send(search_battle)
+                # Uncomment to challenge a user to a battle
+                search_battle = "|/challenge br3nr, gen7randombattle"
+                await websocket.send(search_battle)
 
 if __name__ == "__main__":
     asyncio.run(showdown_client())
+
