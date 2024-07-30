d89749c38bb628ef1a4a38450f94489e8ec614a4
br3nr <repetitive_routine@protonmail.com>
2024-07-30 19:51:04 +0800

    Remove RQID from turn payloads

--- classes/client.py
+++ classes/client.py
@@ -17,11 +17,12 @@ class ShowdownClient:
         self.username = username
         self.password = password
         self.opponent = opponent
-        self.turn = 0
         self.trainer = None
         self.websocket = None
-        self.model = genai.GenerativeModel(model_name='gemini-1.5-flash-001', tools=[self.choose_move])
-    
+        self.model = genai.GenerativeModel(
+            model_name="gemini-1.5-flash-001", tools=[self.choose_move]
+        )
+
     def choose_move(self, move_name: str):
         """Trigger the next move to be used"""
         move_id = 0
@@ -29,37 +30,33 @@ class ShowdownClient:
         for i, move in enumerate(moves):
             if move_name == move["move"]:
                 move_id = i
-        print("Fetched move id", move_id)
-        return move_id+1
+        return move_id + 1
 
     def get_current_moves(self):
         """Gets the available moves for your active pokemon"""
+        # TODO: Check move isnt disabled = True
         active_pokemon = self.trainer.get_active_pokemon()
         moves = self.trainer.active_moves
         move_str = ""
 
         for move in moves:
-            move_name = move["move"].lower().replace(" ","-")
+            move_name = move["move"].lower().replace(" ", "-")
             move_url = f"https://pokeapi.co/api/v2/move/{move_name}"
-            print(move_url)
             try:
                 move_data = requests.get(move_url).json()
                 for effect in move_data["effect_entries"]:
                     if effect["language"]["name"] == "en":
-                        print(effect["short_effect"])
-                        print("acc", move_data["accuracy"])
-                        print("class", move_data["damage_class"]["name"])
+                        pass
                 move_str += f"{move['move']}, "
             except Exception as e:
                 pass
         return move_str
 
-
     async def battle_loop(self, websocket, message):
         chat = self.model.start_chat(enable_automatic_function_calling=True)
         turn_stats = str(message).split("\n")
         self.battle_id = turn_stats[0][1:]
-        if "|request|" in str(message): #and "active" in str(message):
+        if "|request|" in str(message):  # and "active" in str(message):
             # get the player and team data
             try:
                 pokemon_stats = turn_stats[1].replace("|request|", "")
@@ -70,25 +67,21 @@ class ShowdownClient:
                         name=player_dict["side"]["name"],
                         id=player_dict["side"]["id"],
                         team=team,
-                        active_moves=player_dict["active"][0]["moves"]
+                        active_moves=player_dict["active"][0]["moves"],
                     )
-                    p = self.trainer.get_active_pokemon()
-                    print(p.get_ability_info())
-                elif "force_switch" in player_dict:
-                    self.turn += 2
+
+                elif "forceSwitch" in player_dict:
                     id = self.trainer.get_next_available()
                     if id:
-                        payload = f"{self.battle_id}|/choose switch {id}|{self.turn}"
+                        payload = f"{self.battle_id}|/choose switch {id}"
+                        print(payload)
                         await websocket.send(payload)
             except Exception as e:
                 print(e)
 
         elif "|turn|" in turn_stats[len(turn_stats) - 1]:
-            # Do some attack
-            self.turn += 2
-            print("\n\n\n\n\n")
             chat = self.model.start_chat(enable_automatic_function_calling=True)
-            moves = self.get_current_moves() 
+            moves = self.get_current_moves()
             msg = f"""
                 You are in a pokemon battle. You must select a move. 
                 These are your moves: 
@@ -98,19 +91,12 @@ class ShowdownClient:
 
                 Return the single int id.
             """
-            
-            re = chat.send_message(msg).text
-            
-            payload = f"{self.battle_id}|/choose move {int(re)}|{self.turn}"
-            
-            await self.websocket.send(payload)
 
-        elif str(message).endswith("|upkeep") and "faint" in str(message):
-            self.turn += 2
-            # p1 or p2 pokemon fainted
-            # TODO: refine detection
-            #print("the moves", self.get_current_moves())
-            # faint|p2a when other plaer
+            re = chat.send_message(msg).text
+
+            payload = f"{self.battle_id}|/choose move {int(re)}"
+            print(payload)
+            await self.websocket.send(payload)
 
     async def get_challenge_data(self, challstr):
         payload = {
@@ -152,6 +138,7 @@ class ShowdownClient:
                 )
             )
         return team
+
     async def showdown_client(self):
         # Must redefine User-Agent to prevent showdown auto-ban
         # as specified by showdown mods
@@ -159,15 +146,16 @@ class ShowdownClient:
         url = "wss://sim3.psim.us/showdown/websocket"
 
         self.websocket = await websockets.connect(url, extra_headers=headers)
-     
+
         while True:
             # Wait for any incoming message or a timeout
             task = asyncio.wait_for(self.websocket.recv(), timeout=3000)
             message = await task
+            print(message)
+
             if "challstr" in str(message):
                 await self.authenticate(self.websocket, message)
                 search_battle = f"|/challenge {self.opponent}, gen7randombattle"
                 await self.websocket.send(search_battle)
             elif str(message).startswith(">battle"):
                 await self.battle_loop(self.websocket, message)
-
