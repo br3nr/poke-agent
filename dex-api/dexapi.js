10122381c59d6a6ec6477b494b6fe4ef1ad658f9
br3nr <repetitive_routine@protonmail.com>
2025-05-18 22:25:36 +0800

    Return basic move and pokemon data

--- dex-api/dexapi.js
+++ dex-api/dexapi.js
@@ -2,25 +2,49 @@ import express from 'express';
 import { Dex } from '@pkmn/dex';
 
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-app.get('/pokemon/:name', (req, res) => {
-    const pokemonName = req.params.name;
-    const species = Dex.species.get(pokemonName);
-    
-    if (!species) {
-        return res.status(404).json({ error: `Pokemon '${pokemonName}' not found.` });
-    }
-    
+app.get('/pokemon', (req, res) => {
+    const name = req.query.name;
+    if (!name) return res.status(400).json({ error: 'Pls provide pokemon name in query' });
+
+    const species = Dex.species.get(name);
+    if (!species) return res.status(404).json({ error: `'${name}' not found` });
+
     res.json({
         name: species.name,
         types: species.types,
         baseStats: species.baseStats,
-        abilities: Object.values(species.abilities)
+        abilities: species.abilities,
+        weight: species.weightkg
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.get('/move', (req, res) => {
+    const name = req.query.name;
+    if (!name) return res.status(400).json({ error: 'Pls provide move name in query' });
+
+    const move = Dex.moves.get(name);
+    if (!move) return res.status(404).json({ error: `'${name}' not found` });
+
+    res.json({
+        accuracy: move.accuracy,
+        basePower: move.basePower,
+        category: move.category,
+        moveName: move.name,
+        pp: move.pp,
+        flags: move.flags,
+        critRatio: move.critRatio,
+        type: move.type,
+        desc: move.desc,
+        shortDesc: move.shortDesc,
+        condition: move.condition,
+        priority: move.priority,
+        isZ: move.isZ
+    });
+});
+
+app.listen(port, () => {
+    console.log(`running at http://localhost:${port}`);
 });
 
